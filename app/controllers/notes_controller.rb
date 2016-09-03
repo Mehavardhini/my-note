80893f4dc132db569d616e31200e10cf7b189cd2
Mehavardhini <vmehavardhini@gmail.com>
2016-09-03 02:21:12 -0500

    CRUD operation for Note

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,50 @@
+class NotesController < ApplicationController
+
+  before_action :find_note, only: [:edit, :update, :destroy, :show]
+
+  def index
+    @notes = Note.all.order("created_at DESC")
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+    if @note.save
+      redirect_to @note
+    else
+      render "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @note.update(note_params)
+      redirect_to @note
+    else
+      render "edit"
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @note.destroy
+    redirect_to root_path
+  end
+
+  private
+  def note_params
+    params.require(:note).permit(:title, :content)
+  end
+
+  def find_note
+    @note = Note.find(params[:id])
+  end
+
+end
